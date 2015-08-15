e969d018b9d438e7662b4d1486252c37e6da4422
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-08-15 16:41:34 -0400

    Deleted misplaced test

--- src/projecteuler/problems/4_test.clj
+++
@@ -1,7 +0,0 @@
-(ns projecteuler.problems.1-test
-  (:require [clojure.test :refer :all]
-            [projecteuler.problems.1 :refer :all]))
-
-(deftest problem-1-test
-  (testing "Problem 1"
-    (is (= (problem-1 [3 5] 1000) 233168))))
