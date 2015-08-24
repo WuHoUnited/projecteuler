899acbc28ca9f550e0323001fd80ac1b1dcc5890
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-08-24 18:51:30 -0400

    Problem 35

---
+++ src/projecteuler/problems/35.clj
@@ -0,0 +1,45 @@
+(ns projecteuler.problems.35
+  (:require [projecteuler.problems.library :as lib]))
+
+(defn rotate-coll [coll]
+  (as-> coll it
+    (cons (last it) coll)
+    (butlast it)))
+
+(defn get-rotations [coll]
+  (let [length (count coll)]
+    (->> coll
+         (iterate rotate-coll)
+         (take length))))
+
+(defn get-rotations-number [n]
+  (->> n
+       lib/digits 
+       get-rotations
+       (map (partial apply str))
+       (map read-string)))
+
+(defn contains-zero? [n]
+  (contains? (set (lib/digits n)) 0))
+
+(defn circular-prime? [n]
+  (let [digits (lib/digits n)
+        length (count digits)]))
+
+(defn all-rotations-prime? [n]
+  (->> n
+       get-rotations-number
+       rest
+       (every? lib/prime?)))
+
+(defn problem-35
+  ([]
+   (problem-35 1e6))
+  ([limit]
+   (->> lib/primes
+        (take-while #(< % limit))
+        (filter (complement contains-zero?))
+        (filter all-rotations-prime?)
+        count)))
+
+#_(problem-35)
