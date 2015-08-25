78bd7bad6330f66a48ce6f445e0407f5992a9354
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-08-25 18:26:12 -0400

    Problem 40

---
+++ src/projecteuler/problems/40.clj
@@ -0,0 +1,22 @@
+(ns projecteuler.problems.40
+  (:require [projecteuler.problems.library :as lib]))
+
+(defn champernown-digits []
+  (->> (range)
+       rest
+       (mapcat lib/digits)))
+
+(defn get-champernown-index [index]
+  (nth (champernown-digits)
+       (dec index)))
+
+(defn problem-40
+  ([]
+   (problem-40 7))
+  ([total-terms]
+   (->> (range total-terms)
+        (map (partial lib/pow 10))
+        (map get-champernown-index)
+        lib/product)))
+
+#_(problem-40)
