3b908010d87a7d27526c9e81b407f0853a8a2095
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-08-30 18:23:27 -0400

    Problem 39 & add numeric tower

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/WuHoUnited/projecteuler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
---
+++ src/projecteuler/problems/39.clj
@@ -0,0 +1,25 @@
+(ns projecteuler.problems.39
+  (:require [projecteuler.problems.library :as lib]
+            [clojure.math.numeric-tower :as math]))
+
+(defn generate-triangles [perimeter-limit]
+  (for [a (range 1 (quot perimeter-limit 2))
+        b (iterate inc a)
+        :while (<= (+ a b b) perimeter-limit)
+        :let [[c rem] (math/exact-integer-sqrt (+ (lib/square a)
+                                                  (lib/square b)))]
+        :when (zero? rem)]
+    [a b c]))
+
+(defn problem-39
+  ([]
+   (problem-39 1000))
+  ([limit]
+   (->> (generate-triangles limit)
+        (filter (partial apply lib/pythagorean?))
+        (map (partial apply +))
+        frequencies
+        (apply max-key second)
+        first)))
+
+#_(problem-39)
