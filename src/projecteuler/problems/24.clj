35a592883b7582d431d45698bd3bac8b2bba2562
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-08-30 16:18:08 -0400

    Problem 24

---
+++ src/projecteuler/problems/24.clj
@@ -0,0 +1,33 @@
+(ns projecteuler.problems.24
+  (:require [projecteuler.problems.library :as lib]))
+
+(defn remove-index [index coll] 
+  (let [[beginning ending] (split-at index coll)]
+    (concat beginning (rest ending))))
+
+(defn permutation [items permutation-index]
+  (loop [items items
+         permutation-index permutation-index
+         acc []]
+    (let [size (count items)]
+      (if (zero? size)
+        acc
+        (let [items-per-first-item (lib/factorial (dec size))
+              first-item-index (quot permutation-index items-per-first-item)
+              remaining-permutation (- permutation-index
+                                       (* items-per-first-item
+                                          first-item-index))
+              first-item (nth items first-item-index)
+              remaining-items (remove-index first-item-index items)]
+          (recur remaining-items
+                 remaining-permutation
+                 (conj acc first-item)))))))
+
+(defn problem-24
+  ([]
+   (problem-24 (range 10) 1e6))
+  ([items number]
+   (->> (permutation items (dec number))
+        (apply str))))
+
+#_(problem-24)
