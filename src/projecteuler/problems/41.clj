baf7282379a4fb6a01575ec0547492f935d33a75
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-08-31 19:16:21 -0400

    Problem 41

---
+++ src/projecteuler/problems/41.clj
@@ -0,0 +1,22 @@
+(ns projecteuler.problems.41
+  (:require [projecteuler.library :as lib]))
+
+(def limit
+  "This is the largest value we have to check.
+  We know that it can be at most 7 digits because all pandigital
+  8 and 9 digit numbers are divisible by 3 because the sum of their digits
+  is divisible by 3 and therefore not prime."
+  (dec (lib/pow 10 7)))
+
+(defn pandigital? [n]
+  (let [digs (lib/digits n)]
+    (= (sort digs)
+       (range 1 (inc (count digs))))))
+
+(defn problem-41 []
+  (->> (range limit 1 -1)
+       (filter #(and (pandigital? %)
+                     (lib/prime? %)))
+       first))
+
+#_(problem-41)
